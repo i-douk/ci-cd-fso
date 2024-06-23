815a65dfcdc66f4df47daab08358e0d46c63706a
ihssandk <ihssandoukali@gmail.com>
2024-06-23 17:58:22 +0100

    11.6

---
+++ full-stack-open-pokedex/eslint.config.mjs
@@ -0,0 +1,42 @@
+import globals from "globals"
+import stylisticJs from '@stylistic/eslint-plugin-js'
+import js from '@eslint/js'
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+    },
+    ignores: [
+      "webpack.config.js",
+      "node_modules/",
+      "dist/"
+    ],
+  },
+]
